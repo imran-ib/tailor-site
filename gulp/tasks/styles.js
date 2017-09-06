2010dc3c08e0660ef3d19cb4f0e3ff2f7a853053
Imran <imran-ib@outlook.com>
2017-09-06 22:00:20 +0300

    gulp setup finish

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,24 @@
+var gulp = require("gulp");
+var postcss = require("gulp-postcss");
+var atImport = require("postcss-import");
+var mixins = require("postcss-mixins");
+var nested = require("postcss-nested");
+var cssVars = require("postcss-simple-vars");
+var hexrgba = require("postcss-hexrgba");
+var autoprefixer= require("autoprefixer");
+
+
+gulp.task("styles",function(){
+	
+	return gulp.src("./app/assets/styles/styles.css")
+			.pipe(postcss([
+				atImport,
+				cssVars,
+				mixins,
+				nested,
+				hexrgba,
+				autoprefixer
+				
+				]))
+			.pipe(gulp.dest("./app/temp/styles"));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,32 @@
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var browserSync = require("browser-sync").create();
+
+
+
+gulp.task("watch",function(){
+	browserSync.init({
+		server:{
+			baseDir:"app"
+		}
+	});
+	
+
+
+	watch("./app/index.html",function  (argument) {
+		browserSync.reload();
+	});
+
+
+
+
+	watch("./app/assets/styles/**/*.css",function  (argument) {
+		gulp.start("cssInject");
+	})
+})
+
+
+gulp.task("cssInject",["styles"], function(){
+	return gulp.src("./app/assets/styles/styles.css")
+		.pipe(browserSync.stream());
+})
